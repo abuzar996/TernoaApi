3aba23232ea31acf10b5f32c636b0fd76a568441
Leouarz <ghalielouarzazi@hotmail.com>
2021-09-29 12:48:58 +0200

    add transaction

--- src/api/faucet/controller.ts
+++ src/api/faucet/controller.ts
@@ -1,28 +1,53 @@
 import { NextFunction, Request, Response } from "express";
 import { IFaucetClaim } from "../../interfaces/IFaucetClaim";
 import faucetClaimModel from "../../models/faucetClaim";
+import { getKeyring, getChainApiInstance, unFormatBalance, isValidAddress } from "../../utils/polka";
+import { DEFAULT_CAPS_AMOUNT } from "../../utils";
+
+const parseArgs = require('minimist')(process.argv.slice(2))
+const SEED=parseArgs["SEED"] ? parseArgs["SEED"] : null
 
 export class Controller {
     async claimTestCaps(req: Request, res: Response, next: NextFunction): Promise<void> {
         try{
             const { walletId } = req.params
+            // Check address validity
+            if (walletId.length !== 48 || !isValidAddress(walletId)) throw new Error('Invalid address format')
             //Check if claim is possible
-            if (walletId.length !== 48) throw new Error('Invalid address formar')
             const lastClaim = await faucetClaimModel.findOne({walletId}).sort({createdAt: -1})
             if (lastClaim && lastClaim.createdAt){
                 const timeDiff = new Date().getTime() - lastClaim.createdAt.getTime()
                 if (timeDiff < (2 * 24 * 3600 * 1000)){
                     const timeLeftForNextClaim = (2 * 24 * 3600 * 1000) - timeDiff
-                    res.status(403).json(`You need to wait ${timeLeftForNextClaim} before making another claim`)
+                    let err = (new Error(`You need to wait ${timeLeftForNextClaim} before making another claim`)) as any
+                    err.status = 403
+                    next(err)
                 }
             }
             // CLAIM HERE FROM BC
-            
-            //Add claim in DB
-            const claim: IFaucetClaim = { walletId }
-            const claimDB = new faucetClaimModel(claim)
-            await claimDB.save()
-            res.status(200).json({message: `Successfully claimed for address ${walletId}`, claim: claim});
+            const keyring = await getKeyring()
+            if (SEED && keyring){
+                const sender = keyring.createFromUri(SEED)
+                let api = await getChainApiInstance()
+                let extrinsic = api.tx.balances.transferKeepAlive(walletId, unFormatBalance(DEFAULT_CAPS_AMOUNT));
+                const unsub = await extrinsic.signAndSend(sender, async (result: any) => {
+                    if (result.status.isInBlock) {
+                        console.log(`Transaction included at blockHash ${result.status.asInBlock}`);
+                        unsub();
+                        /*if (result. is ok){
+
+                        }*/
+                        //Add claim in DB
+                        const claim: IFaucetClaim = { walletId }
+                        const claimDB = new faucetClaimModel(claim)
+                        await claimDB.save()
+                        res.status(200).json({message: `Successfully claimed for address ${walletId}`, claim: claim});
+                    }
+                })
+            }else{
+                let err = (new Error(`An error has occured when trying to get you caps, please try again later.`)) as any
+                next(err)
+            }
         }catch(err){
             next(err)
         }
--- src/utils/polka/index.ts
+++ src/utils/polka/index.ts
@@ -1,18 +1,60 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const { types } = require('./types');
+import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { BN_TEN, hexToU8a, isHex } from '@polkadot/util';
+import { decodeAddress, encodeAddress } from '@polkadot/keyring';
+import types from './types'
+import BN from 'bn.js';
+
+
+const provider = new WsProvider(process.env.BLOCK_CHAIN_URL);
+const typesConverted = types as any
 
 let api: any = null;
-const provider = new WsProvider(process.env.BLOCK_CHAIN_URL);
-console.log('BLOCK_CHAIN_URL', process.env.BLOCK_CHAIN_URL)
-
-async function getChainApiInstance() {
-    
+export const getChainApiInstance = async () => {
     if (api && api.isConnected) {
         return api;
     } else {
-        api = await ApiPromise.create({ provider, types });
+        api = await ApiPromise.create({ provider, types: typesConverted });
         return api;
     }
 };
 
-export default getChainApiInstance
+export const getKeyring = async () => {
+    await cryptoWaitReady();
+    return new Keyring({ type: 'sr25519' });
+}
+
+export const unFormatBalance = (_input: number) => {
+    const input = '' + _input;
+    const siPower = new BN(api.registry.chainDecimals[0]);
+    const basePower = api.registry.chainDecimals[0];
+    const siUnitPower = 0;
+    const isDecimalValue = input.match(/^(\d+)\.(\d+)$/);
+  
+    let result;
+  
+    if (isDecimalValue) {
+      if (siUnitPower - isDecimalValue[2].length < -basePower) {
+        result = new BN(-1);
+      }
+      const div = new BN(input.replace(/\.\d*$/, ''));
+      const modString = input.replace(/^\d+\./, '').substr(0, api.registry.chainDecimals[0]);
+      const mod = new BN(modString);
+  
+      result = div.mul(BN_TEN.pow(siPower)).add(mod.mul(BN_TEN.pow(new BN(basePower + siUnitPower - modString.length))));
+    } else {
+      result = new BN(input.replace(/[^\d]/g, '')).mul(BN_TEN.pow(siPower));
+    }
+    console.log('unformat balance result', result);
+    return result;
+}
+
+export const isValidAddress = (address: string) => {
+    try {
+        encodeAddress(isHex(address) ? hexToU8a(address) : decodeAddress(address));
+        return true;
+    }catch(error) {
+        return false;
+    }
+}
+
