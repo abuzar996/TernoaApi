8aa6fa7c9b338e32a24dbc5f496046af2dae1700
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2021-10-03 20:09:24 +0200

    default caps amount to 150

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { decodeAddress, signatureVerify } from '@polkadot/util-crypto';
 import { u8aToHex } from '@polkadot/util';
 
 export const LIMIT_MAX_PAGINATION = 50
-export const DEFAULT_CAPS_AMOUNT = 20
+export const DEFAULT_CAPS_AMOUNT = 150
 export const DEFAULT_FAUCET_BATCH_SIZE = 1000
 
 export const validateEmail = (mail: string) => {
