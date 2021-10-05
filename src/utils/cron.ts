28cb1b0c62417b59c9d7c4676642e40273d5b3eb
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2021-10-05 17:14:40 +0200

    change cron to 1 mn

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -2,6 +2,6 @@ import cron from 'node-cron'
 import faucetClaimService from '../services/faucetClaim'
 
 // Cronjob to process faucet claims by batch of 1k each 15 seconds
-export default cron.schedule('*/15 * * * * *', function() {
+export default cron.schedule('* * * * *', function() {
     faucetClaimService.processQueue()
 });
