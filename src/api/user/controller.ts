81ff11c76f5b00a3d417d2a61837541d6be9aa85
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2021-10-11 12:33:03 +0200

    add log

--- src/api/user/controller.ts
+++ src/api/user/controller.ts
@@ -28,6 +28,7 @@ export class Controller {
             const { walletId } = JSON.parse(body);
             let existingUser = null;
             existingUser = await UserModel.findOne({ walletId });
+            console.log("existing user", existingUser)
             if (existingUser) {
                 let err = new Error('Wallet user already exists') as any
                 err.status=409
